c180fa749e30a1a801b9948289b5e2d134f9a385
kidy <zjindiss@gmail.com>
2016-04-07 20:16:48 +0800

    验证开发者

---
+++ wechat_auth/views.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import hashlib
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+ 
+WEIXIN_TOKEN = 'lvxingjiakidy'
+ 
+@csrf_exempt
+def index(request):
+    if request.method == "GET":
+        signature = request.GET.get("signature", None)
+        timestamp = request.GET.get("timestamp", None)
+        nonce = request.GET.get("nonce", None)
+        echostr = request.GET.get("echostr", None)
+        token = WEIXIN_TOKEN
+        tmp_list = [token, timestamp, nonce]
+        tmp_list.sort()
+        tmp_str = "%s%s%s" % tuple(tmp_list)
+        tmp_str = hashlib.sha1(tmp_str).hexdigest()
+        if tmp_str == signature:
+            return HttpResponse(echostr)
+        else:
+            return HttpResponse("weixin  index")
