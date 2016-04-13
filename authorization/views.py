c103f6de5ea49a03a48aa24ad52685b7462b940b
kidy <zjindiss@gmail.com>
2016-04-13 19:51:24 +0800

    add user_info page, more pep8

--- authorization/views.py
+++ authorization/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import hashlib
-import time 
+import time
 import urllib
 import json
 
@@ -10,8 +10,9 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 WEIXIN_TOKEN = 'lvxingjiakidy'
- 
-from wechat_auth.settings import appID, appsecret 
+
+from wechat_auth.settings import appID, appsecret
+
 
 @api_view(['GET', 'POST'])
 def index(request):
@@ -43,14 +44,15 @@ def index(request):
         content = data.get('Content', '')
 
         return render(request, 'reply.xml',
-                    {  'toUserName': fromUserName,
+                      {'toUserName': fromUserName,
                        'fromUserName': toUserName,
                        'createTime': int(time.time()),
                        'msgType': msgType,
                        'content': content,
-                    },
-            content_type='application/xml'
-        )
+                       },
+                      content_type='application/xml'
+                      )
+
 
 def web_auth(request):
     """
@@ -68,12 +70,12 @@ def web_auth(request):
                 'response_type': 'code',
                 'scope': 'snsapi_userinfo',
                 'state': 'STATE',
-                })
-            redirect_url = 'https://open.weixin.qq.com/connect/oauth2/authorize?' + data + '#wechat_redirect'
+            })
+            redirect_url = 'https://open.weixin.qq.com/connect/oauth2/authorize?' + \
+                data + '#wechat_redirect'
             return redirect(redirect_url)
         else:
             pass
-
 
 
 @api_view(['GET'])
@@ -87,61 +89,66 @@ def get_code(request):
             # 第二步：通过code换取网页授权access_token
             try:
                 response = urllib.urlopen(url='https://api.weixin.qq.com/sns/oauth2/access_token',
-                    data=urllib.urlencode({
-                    'appid': appID,
-                    'secret': appsecret,
-                    'code': code,
-                    'grant_type': 'authorization_code',
-                    })
-                )
+                                          data=urllib.urlencode({
+                                              'appid': appID,
+                                              'secret': appsecret,
+                                              'code': code,
+                                              'grant_type': 'authorization_code',
+                                          })
+                                          )
             except Exception, reson:
                 print reson
 
             response_dic = json.loads(response.read())                 # 解析json
             errcode = response_dic.get('errcode', None)         # 调用失败
             if errcode:
-                raise Warning(str(errcode) + ':' + response_dic.get('errmsg', ''))   # raise 错误原因
-            
+                # raise 错误原因
+                raise Warning(
+                    str(errcode) + ':' + response_dic.get('errmsg', ''))
+
             print response_dic
-            
+
             access_token = response_dic.get('access_token', '')
             expires_in = response_dic.get('expires_in', '')
             refresh_token = response_dic.get('refresh_token', '')
             openid = response_dic.get('openid', '')
             scope = response_dic.get('scope', '')
 
-
             # 第四步：拉取用户信息(需scope为 snsapi_userinfo)
             info_url = 'https://api.weixin.qq.com/sns/userinfo'
             try:
                 info_response = urllib.urlopen(url=info_url,
-                    data=urllib.urlencode({
-                    'access_token': access_token,
-                    'openid': openid,
-                    'lang':'zh_CN'       # 默认使用中文吧                        
-                    })
-                )
+                                               data=urllib.urlencode({
+                                                   'access_token': access_token,
+                                                   'openid': openid,
+                                                   # 默认使用中文吧
+                                                   'lang': 'zh_CN'
+                                               })
+                                               )
             except Exception, reson:
                 print reson
 
             info_dic = json.loads(info_response.read())          # 解析json
             errcode = info_dic.get('errcode', None)              # 调用失败
             if errcode:
-                raise Warning(str(errcode) + ':' + response_dic.get('errmsg', ''))   # raise 错误原因
+                # raise 错误原因
+                raise Warning(
+                    str(errcode) + ':' + response_dic.get('errmsg', ''))
 
             nickname = info_dic.get('nickname', '')
-            sex = info_dic.get('sex', '')
+            sex = info_dic.get('sex', 0)
             province = info_dic.get('province', '')
-            city = info_dic.get('city', '')
             country = info_dic.get('country', '')
             city = info_dic.get('city', '')
             headimgurl = info_dic.get('headimgurl', '')
 
-            print info_dic
-
-            return render(request, 'reply.xml',
-                    {  
-                       'content': 'ok',
-                    },
-            content_type='application/xml'
-           )
+            return render(request, 'user_info.html',
+                          {
+                              'nickname': nickname,
+                              'sex': sex,
+                              'province': province,
+                              'country': country,
+                              'city': city,
+                              'headimgurl': headimgurl,
+                          },
+                          )
